a96631fe4df6f68b61b4f5bf5c7209b777ed8fa5
Kumar Gaurav <kmgaurav2611@gmail.com>
2020-04-12 13:48:09 +0530

    Updates

---
+++ precache-manifest.607861963d8ae6eebf0c2924774a1314.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9f301247b3911f07eb4a920fddbcd0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "03c4b32de95025fa7153",
+    "url": "/monsters-rolodex/static/css/main.9dac3076.chunk.css"
+  },
+  {
+    "revision": "105422028b6763bd0844",
+    "url": "/monsters-rolodex/static/js/2.f35d3ce1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.f35d3ce1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03c4b32de95025fa7153",
+    "url": "/monsters-rolodex/static/js/main.aa257742.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
