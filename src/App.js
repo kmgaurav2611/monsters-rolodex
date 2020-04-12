fd182162c963eb64eb32de4af02203c46624c61a
Kumar Gaurav <kmgaurav2611@gmail.com>
2020-04-12 12:39:02 +0530

    final commit before deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
 import "./App.css";
 
 class App extends Component {
@@ -7,7 +10,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: "",
     };
   }
 
@@ -15,18 +19,25 @@ class App extends Component {
     const response = async () => {
       const res = await fetch("https://jsonplaceholder.typicode.com/users");
       const users = await res.json();
+      this.setState({ monsters: users }, () => console.log("success"));
       console.log("usr", users);
     };
     response();
   }
   render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <header className="App-header">
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </header>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
